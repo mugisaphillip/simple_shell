2dae52ec64bac7727722c08430bff5a932806646
phillip mugisawq <phillipmugisa@icloud.com>
2023-05-22 01:40:32 +0300

    5. Simple shell 1.0

--- shell.c
+++ shell.c
@@ -1,6 +1,48 @@
 #include "shell.h"
 
 /**
+ * runProgram - runs program
+ * @prompt: command entered
+ * @args: command argument
+ * @envp: env variables
+ * @chars_written: characters written
+ * @progName: program name
+ * Return: void
+ */
+void runProgram(char *prompt, char **args, char **envp, ssize_t chars_written, char *progName)
+{
+	char *token = NULL;
+	int n_tokens = 0;
+	char delim[] = " ";
+	int i = 0;
+
+	prompt[chars_written - 1] = '\0';
+	handleExit(prompt, args);
+
+	if (strcmp(prompt, "env") == 0)
+	{
+		while (envp[i] != NULL)
+		{
+			_puts(envp[i]);
+			_putchar('\n');
+			i++;
+		}
+	}
+
+	token = strtok(prompt, delim);
+	while (token != NULL)
+	{
+		args[n_tokens] = token;
+		n_tokens++;
+		token = strtok(NULL, delim);
+	}
+	args[n_tokens] = NULL;
+	if (n_tokens > 0)
+		execute(progName, args, envp);
+	n_tokens = 0;
+}
+
+/**
  * runInteractiveMode - runs if user is typing in commands
  * @envp: environment variables
  * @progName: program name
@@ -13,9 +55,6 @@ void runInteractiveMode(char *progName, char **envp)
 	char *prompt = NULL;
 	size_t len = 0;
 	char **args;
-	char *token = NULL;
-	int n_tokens = 0;
-	char delim[] = " ";
 
 	args = malloc(sizeof(char *) * MAXWORDS);
 	while (1)
@@ -24,21 +63,7 @@ void runInteractiveMode(char *progName, char **envp)
 		chars_written = getline(&prompt, &len, stdin);
 		if (chars_written == -1)
 			break;
-
-		prompt[chars_written - 1] = '\0';
-		handleExit(prompt, args);
-
-		token = strtok(prompt, delim);
-		while (token != NULL)
-		{
-			args[n_tokens] = token;
-			n_tokens++;
-			token = strtok(NULL, delim);
-		}
-		args[n_tokens] = NULL;
-		if (n_tokens > 0)
-			execute(progName, args, envp);
-		n_tokens = 0;
+		runProgram(prompt, args, envp, chars_written, progName);
 	}
 
 	free(args);
@@ -58,27 +83,11 @@ void runNonInteractiveMode(char *progName, char **envp)
 	char *prompt = NULL;
 	size_t len = 0;
 	char **args;
-	char *token = NULL;
-	int n_tokens = 0;
-	char delim[] = " ";
 
 	args = malloc(sizeof(char *) * MAXWORDS);
 	chars_written = getline(&prompt, &len, stdin);
 
-	prompt[chars_written - 1] = '\0';
-	handleExit(prompt, args);
-
-	token = strtok(prompt, delim);
-	while (token != NULL)
-	{
-		args[n_tokens] = token;
-		n_tokens++;
-		token = strtok(NULL, delim);
-	}
-	args[n_tokens] = NULL;
-	if (n_tokens > 0)
-		execute(progName, args, envp);
-	n_tokens = 0;
+	runProgram(prompt, args, envp, chars_written, progName);
 
 	free(args);
 	free(prompt);
