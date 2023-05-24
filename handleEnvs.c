cecfdcf1a464d2cd04f4cd6f80ba1ffe5cdf1241
phillip mugisawq <phillipmugisa@icloud.com>
2023-05-25 00:45:34 +0300

    9. setenv, unsetenv

--- handleEnvs.c
+++ handleEnvs.c
@@ -14,17 +14,27 @@ int handleEnvs(char **args)
 		if (args[1] == NULL || args[2] == NULL)
 		{
 			perror("few arguments placed");
+			return (1);
 		}
-		else if (setenv(args[1], args[2], 1) != 0)
+		if (setenv(args[1], args[2], 1) != 0)
+		{
 			perror("variable not set");
+			return (1);
+		}
 		status = 1;
 	}
 	else if (strcmp(args[0], "unsetenv") == 0)
 	{
 		if (args[1] == NULL)
+		{
 			perror("few arguments placed");
-		else if (unsetenv(args[1]) != 0)
+			return (1);
+		}
+		if (unsetenv(args[1]) != 0)
+		{
 			perror("variable not unset");
+			return (1);
+		}
 		status = 1;
 	}
 	return (status);
